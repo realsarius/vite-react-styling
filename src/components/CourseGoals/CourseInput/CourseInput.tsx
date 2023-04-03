5b1ca94da74600785f128043d3abf8e2d45a9a2c
realsarius <54468228+realsarius@users.noreply.github.com>
2023-04-03 20:29:34 +0300

    added validation check for input

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -4,26 +4,41 @@ import './CourseInput.scss';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control my-2">
-        <label className="font-bold block my-2 antialiased text-lg text-white">
+      <div className={`form-control my-2 ${!isValid ? 'invalid' : ''}`}>
+        <label
+          className={`font-bold block my-2 antialiased text-lg transition-colors duration-300 ${
+            !isValid ? 'text-red-500' : 'text-white'
+          }`}
+        >
           Course Goal
         </label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
-          className="block antialiased font-sans w-full text-inherit line leading-6 px-1 py-1 rounded-md border-solid border-2 transition-colors duration-200 focus:outline-none focus:bg-input-color focus:border-input-border-color"
+          className={`block antialiased font-sans w-full text-inherit line leading-6 px-1 py-1 rounded-md border-solid border-2 transition-colors duration-300 focus:outline-none focus:bg-input-color focus:border-input-border-color ${
+            !isValid ? 'border-red-400 bg-[#ffbaba]' : ''
+          }`}
         />
       </div>
       <Button type="submit">Add Goal</Button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
         'dark-purple-bg-focus': '#b35d8b',
         'input-color': '#fde8f6',
         'input-border-color': '#cb8bab',
+        'bg-invalid': 'ffd7d7',
         boxShadow: {
           custom:
             'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
