0f7e364576781c5e1fbe7d001addae7904667f4c
realsarius <54468228+realsarius@users.noreply.github.com>
2023-04-03 20:28:17 +0300

    fixed tailwind-webpack-plugin-utils error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { TailwindCSSVitePlugin } from 'tailwindcss-vite-plugin';
+// import { TailwindCSSVitePlugin } from 'tailwindcss-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TailwindCSSVitePlugin()],
+  plugins: [react()],
 });
